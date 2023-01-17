a9ada8f1c3902a687f43eaa37d2b05e496dfedd8
IanNathanielLove <ian@steeleconsult.com>
2023-01-17 16:40:19 -0700

    new api version

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,9 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { observable } from 'rxjs';
+import { Observable, observable, Subject, Subscription, takeUntil } from 'rxjs';
 import { PokeService } from '../services/poke.service';
 import { Pokemon } from '../pokemon';
-import { pokeimages, pokemodel, pokemodelPage } from '../models/pokemodel';
-import { ActivatedRoute } from '@angular/router';
+import { pokeimages, pokemodel, pokemodelPage, basicDetails } from '../models/pokemodel';
+import { ActivatedRoute, Router } from '@angular/router';
 
 
 @Component({
@@ -16,57 +16,46 @@ import { ActivatedRoute } from '@angular/router';
 export class ListComponent implements OnInit{
 
   constructor(private pokeSerice: PokeService,
-              private route: ActivatedRoute){
+              private route: ActivatedRoute,
+              private router: Router
+              ){
     
 }
 
+  pokemonPageSub!: Subscription;
   pokemonPage?: pokemodelPage;
+  basicdetails?: basicDetails;
+  unsubscribe$ = new Subject<void>(); 
   pokeImages: pokeimages | undefined;
   pagination!: {nextLink?: string, prevLink?: string, currentPage: number} 
 
   ngOnInit(): void {
-    let pageNum = Number(this.route.snapshot.queryParamMap.get("page") ?? "1");
-    this.pagination = {currentPage: pageNum}
-    this.getPokemany(pageNum);
-    
-   
-
+    let page = Number(this.route.snapshot.queryParamMap.get("page") ?? "1");
+    this.pagination = {currentPage: page}
+    this.getPokemany(page);
+    console.log(page); 
   }
-
-
-  getPokeImages(){
-    for(let pokemon of this.pokemonPage!.pokelist){
-      console.log(pokemon.name);
-      this.pokeSerice.getImages(pokemon.name)
-      .subscribe(pokeImages => pokemon.images = pokeImages)
-    }
-  }
-
 
   getPokemany(page?: number, limit?: number){
-    this.pokeSerice.getPokemon(page, limit)
+    this.router.navigate(["."], {queryParams: {page}})   
+    this.pokemonPageSub = this.pokeSerice.getPokemon(page, limit).pipe(takeUntil(this.unsubscribe$))
       .subscribe(pokemonPage => {
         this.pokemonPage = pokemonPage
-          this.getPokeImages()
       })   
   }
-
+  
 
   loadNext(){ 
-    
     this.getPokemany(this.pokemonPage!.currentPage+1, this.pokemonPage?.limit)
-
   }
 
   loadPrev(){
-
-    
     this.getPokemany(this.pokemonPage!.currentPage-1, this.pokemonPage?.limit)
   }
 
 
- 
-
-
-
+  ngOnDestroy(){
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 }
